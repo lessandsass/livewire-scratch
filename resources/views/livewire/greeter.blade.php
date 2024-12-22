df50b96068c170c0c4710b54725fb0af32c66337
Vipurthanan <lessandsass@gmail.com>
2024-12-22 06:10:38 +0530

    Livewire Actions
    - Define a method called changeName in the Greeter.php component class.
    - The wire:click added in the button
    - we can pass the dynamic value

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,3 +1,16 @@
 <div>
-    Hello {{ $name }}
+
+    <div class="mb-2">
+        Hello {{ $name }}
+    </div>
+
+    <div class="mb-2">
+        <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            wire:click="changeName('Jeffrey')"
+        >
+            Greet
+        </button>
+    </div>
+
 </div>
