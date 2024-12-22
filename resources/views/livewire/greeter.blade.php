9b9dd3d1c906bc89e365e8bea5e2eb4082f346f5
Vipurthanan <lessandsass@gmail.com>
2024-12-22 06:25:53 +0530

    Actions with dynamic input
    - An input element was added.
    - wire:click was implemented with vanilla JS functions,
    - The button type was changed to submit
    - Check the code on the browser.
    - The input and button elements were wrapped up with the form element, wire:click was moved over inside the form element properties and wire:click was changed to wire:submit.
    - Check the output
    -

--- resources/views/livewire/greeter.blade.php
+++ resources/views/livewire/greeter.blade.php
@@ -1,16 +1,29 @@
 <div>
 
-    <div class="mb-2">
-        Hello {{ $name }}
-    </div>
+    <form
+        action=""
+        wire:submit="changeName(document.querySelector('#newName').value)"
+    >
+        <div class="mb-2">
+            Hello {{ $name }}
+        </div>
 
-    <div class="mb-2">
-        <button
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            wire:click="changeName('Jeffrey')"
-        >
-            Greet
-        </button>
-    </div>
+        <div class="mt-2">
+            <input
+                type="text"
+                id="newName"
+                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
+            >
+        </div>
+
+        <div class="mb-2">
+            <button
+                type="submit"
+                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Greet
+            </button>
+        </div>
+    </form>
 
 </div>
