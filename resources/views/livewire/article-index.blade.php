5f8113c01f5c2a75da743f484a7f7ea7ce45d091
Vipurthanan <lessandsass@gmail.com>
2025-01-04 16:56:10 +0530

    some styling polish up
    - some front end styling was added to the article-index and show-article views

--- resources/views/livewire/article-index.blade.php
+++ resources/views/livewire/article-index.blade.php
@@ -1,13 +1,16 @@
-<div>
+<div class="mx-auto w-1/2">
     @foreach ($articles as $article)
         <div class="mt-5 p-3">
-            <h3 class="text-2xl text-blue-500">
-                <a href="/articles/{{ $article->id }}">
+            <h3 class="text-2xl text-blue-500 mb-1 py-1">
+                <a
+                    href="/articles/{{ $article->id }}"
+                    class=hover:text-blue-700 hover:underline"
+                >
                     {{ $article->title }}
                 </a>
             </h3>
 
-            <p>
+            <p class="text-gray-600">
                 {{ $article->content }}
             </p>
         </div>
--- resources/views/livewire/show-article.blade.php
+++ resources/views/livewire/show-article.blade.php
@@ -1,10 +1,10 @@
-<div class="max-h-7xl mx-auto py-10 sm:px-6 lg:px-8">
+<div class="mx-auto w-1/2">
 
-    <h2 class="text-2xl text-gray-500">
+    <h2 class="text-2xl text-blue-500">
         {{ $article->title }}
     </h2>
 
-    <div class="my-4 pb-4">
+    <div class="my-4 pb-4 text-gray-600">
         {{ $article->content }}
     </div>
 
